c55d4f74e1b30f4168317eff3be05dcf8fbb62f1
pinknemo452 <mishanay38@gmail.com>
2021-12-05 01:42:07 +0300

    better console view

--- Board.cpp
+++ Board.cpp
@@ -1,4 +1,5 @@
 #include "Board.h"
+#include <string>
 #include <iostream>
 
 
@@ -25,33 +26,48 @@ void Board::start()
 {
 	model_->Game();
 }
-
+std::string getString(int value,int number) {
+	std::string str2_0;
+	if (value == 3)
+		str2_0 = "\x1b[31m" + std::to_string(number) +"\x1b[0m";
+	if (value == 2)
+		str2_0 = "\x1b[34m" + std::to_string(number) + "\x1b[0m";
+	if (value == 0)
+		str2_0 = "\x1b[37m" + std::to_string(number) + "\x1b[0m";
+	if (number < 10) {
+		if (value == 3)
+			str2_0 = "\x1b[31m" + std::to_string(number) + " \x1b[0m";
+		if (value == 2)
+			str2_0 = "\x1b[34m" + std::to_string(number) + " \x1b[0m";
+		if (value == 0)
+			str2_0 = "\x1b[37m" + std::to_string(number) + " \x1b[0m";
+	}
+	return str2_0;
+}
 void Board::draw() const
 {
-		std::string str2_0;
-		if (board_[2][0] == 3)
-			str2_0 = "\x1b[32m1\x1b[0m";
+		
 
 	std::cout << "\n";
-	std::cout << "                 ___         " << "\n";
-	std::cout << "             ___/ "<<board_[0][4] <<" \\___       " << "\n";
-	std::cout << "         ___/ "<<board_[1][3] <<" \\___/ "<<board_[1][5] <<" \\___     " << "\n";
-	std::cout << "     ___/ "<<board_[1][2] <<" \\___/ "<<board_[1][4] <<" \\___/ "<<board_[1][6] <<" \\___   " << "\n";
-	std::cout << " ___/ "<<board_[2][1] <<" \\___/ "<<board_[2][3] <<" \\___/ "<<board_[2][5] <<" \\___/ "<<board_[2][7] <<" \\___ " << "\n";
-	std::cout << "/ "<</*board_[2][0]*/str2_0 <<" \\___/ "<<board_[2][2] <<" \\___/ "<<board_[2][4] <<" \\___/ "<<board_[2][6] <<" \\___/ "<<board_[2][8] <<" \\" << "\n";
-	std::cout << "\\___/ "<<board_[3][1] <<" \\___/ "<<board_[3][3] <<" \\___/ "<<board_[3][5] <<" \\___/ "<<board_[3][7] <<" \\___/" << "\n";
-	std::cout << "/ "<<board_[3][0] <<" \\___/ "<<board_[3][2] <<" \\___/ "<<board_[3][4] <<" \\___/ "<<board_[3][6] <<" \\___/ "<<board_[3][8] <<" \\" << "\n";
-	std::cout << "\\___/ "<<board_[4][1] <<" \\___/ "<</*board_[4][3]*/ " " <<" \\___/ "<</*board_[4][5]*/ " " <<" \\___/ "<<board_[4][7] <<" \\___/" << "\n";
-	std::cout << "/ "<<board_[4][0] <<" \\___/ "<<board_[4][2] <<" \\___/ "<<board_[4][4] <<" \\___/ "<<board_[4][6] <<" \\___/ "<<board_[4][8] <<" \\" << "\n";
-	std::cout << "\\___/ "<<board_[5][1] <<" \\___/ "<<board_[5][3] <<" \\___/ "<<board_[5][5] <<" \\___/ "<<board_[5][7] <<" \\___/" << "\n";
-	std::cout << "/ "<<board_[5][0] <<" \\___/ "<<board_[5][2] <<" \\___/ "<</*board_[5][4]*/ " " <<" \\___/ "<<board_[5][6] <<" \\___/ "<<board_[5][8] <<" \\" << "\n";
-	std::cout << "\\___/ "<<board_[6][1] <<" \\___/ "<<board_[6][3] <<" \\___/ "<<board_[6][5] <<" \\___/ "<<board_[6][7] <<" \\___/" << "\n";
-	std::cout << "/ "<<board_[6][0] <<" \\___/ "<<board_[6][2] <<" \\___/ "<<board_[6][4] <<" \\___/ "<<board_[6][6] <<" \\___/ "<<board_[6][8] <<" \\" << "\n";
-	std::cout << "\\___/ "<<board_[7][1] <<" \\___/ "<<board_[7][3] <<" \\___/ "<<board_[7][5] <<" \\___/ "<<board_[7][7] <<" \\___/" << "\n";
-	std::cout << "    \\___/ "<<board_[7][2] <<" \\___/ "<<board_[7][4] <<" \\___/ "<<board_[7][6] <<" \\___/  " << "\n";
-	std::cout << "        \\___/ "<<board_[8][3] <<" \\___/ "<<board_[8][5] <<" \\___/    " << "\n";
-	std::cout << "            \\___/ "<<board_[8][4] <<" \\___/      " << "\n";
-	std::cout << "                \\___/        " << "\n";
+	std::cout << "                     ____         " << "\n";
+	std::cout << "                ____/ "<< getString(board_[0][4],5) <<" \\____       " << "\n";
+	std::cout << "           ____/ "<< getString(board_[1][3],4) <<" \\____/ "<< getString(board_[1][5],11) <<" \\____     " << "\n";
+	std::cout << "      ____/ "<< getString(board_[1][2],3) <<" \\____/ "<< getString(board_[1][4],10) <<" \\____/ "<< getString(board_[1][6],18) <<" \\____   " << "\n";
+	std::cout << " ____/ "<< getString(board_[2][1],2) <<" \\____/ "<< getString(board_[2][3],9) <<" \\____/ "<< getString(board_[2][5],17) <<" \\____/ "<< getString(board_[2][7],26) <<" \\____ " << "\n";
+	std::cout << "/ "<</*board_[2][0]*/getString(board_[2][0],1) <<" \\____/ "<< getString(board_[2][2],8) <<" \\____/ "<< getString(board_[2][4],16) <<" \\____/ "<< getString(board_[2][6],25) <<" \\____/ "<< getString(board_[2][8],35) <<" \\" << "\n";
+	std::cout << "\\____/ "<< getString(board_[3][1],7) <<" \\____/ "<< getString(board_[3][3],15) <<" \\____/ "<< getString(board_[3][5],24) <<" \\____/ "<< getString(board_[3][7],34) <<" \\____/" << "\n";
+	std::cout << "/ "<< getString(board_[3][0],6) <<" \\____/ "<< getString(board_[3][2],14) <<" \\____/ "<< getString(board_[3][4],23) <<" \\____/ "<< getString(board_[3][6],33) <<" \\____/ "<< getString(board_[3][8],43) <<" \\" << "\n";
+	std::cout << "\\____/ "<< getString(board_[4][1],13) <<" \\____/ "<</*board_[4][3]*/ "  " <<" \\____/ "<</*board_[4][5]*/ "  " <<" \\____/ "<< getString(board_[4][7],42) <<" \\____/" << "\n";
+	std::cout << "/ "<< getString(board_[4][0],12) <<" \\____/ "<< getString(board_[4][2],21) <<" \\____/ "<< getString(board_[4][4],31) <<" \\____/ "<< getString(board_[4][6],41) <<" \\____/ "<< getString(board_[4][8],50) <<" \\" << "\n";
+	std::cout << "\\____/ "<< getString(board_[5][1],20) <<" \\____/ "<< getString(board_[5][3],30) <<" \\____/ "<< getString(board_[5][5],40) <<" \\____/ "<< getString(board_[5][7],49) <<" \\____/" << "\n";
+	std::cout << "/ "<< getString(board_[5][0],19) <<" \\____/ "<< getString(board_[5][2],29) <<" \\____/ "<</*board_[5][4]*/ "  " <<" \\____/ "<< getString(board_[5][6],48) <<" \\____/ "<< getString(board_[5][8],56) <<" \\" << "\n";
+	std::cout << "\\____/ "<< getString(board_[6][1],28) <<" \\____/ "<< getString(board_[6][3],38) <<" \\____/ "<< getString(board_[6][5],47) <<" \\____/ "<< getString(board_[6][7],55) <<" \\____/" << "\n";
+	std::cout << "/ "<< getString(board_[6][0],27) <<" \\____/ "<< getString(board_[6][2],37) <<" \\____/ "<< getString(board_[6][4],46) <<" \\____/ "<< getString(board_[6][6],54) <<" \\____/ "<< getString(board_[6][8],61) <<" \\" << "\n";
+	std::cout << "\\____/ "<< getString(board_[7][1],36) <<" \\____/ "<< getString(board_[7][3],45) <<" \\____/ "<< getString(board_[7][5],53) <<" \\____/ "<< getString(board_[7][7],60) <<" \\____/" << "\n";
+	std::cout << "     \\____/ "<< getString(board_[7][2],44) <<" \\____/ "<< getString(board_[7][4],52) <<" \\____/ "<< getString(board_[7][6],59) <<" \\____/  " << "\n";
+	std::cout << "          \\____/ "<< getString(board_[8][3],51) <<" \\____/ "<< getString(board_[8][5],58) <<" \\____/    " << "\n";
+	std::cout << "               \\____/ "<< getString(board_[8][4],57) <<" \\____/      " << "\n";
+	std::cout << "                    \\____/        " << "\n";
 }
 
 void Board::decBlueTilesCounterBy(int sub)
